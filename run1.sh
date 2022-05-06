ed9869759c33d498c2afde9633b4f97677970701
superlj666 <superlj666@gmail.com>
2022-05-06 15:10:09 +0800

    DKL experiments

--- run1.sh
+++ run1.sh
@@ -1,2 +1,2 @@
-python3 exp_comparison_DKLModel.py --epochs 300 --repeates 1 --dataset CIFAR100 --gpu 1
+# python3 exp_comparison_DKLModel.py --epochs 300 --repeates 1 --dataset CIFAR100 --gpu 1
 python3 exp_comparison_DKLModel.py --epochs 300 --repeates 1 --dataset TinyImagenet --gpu 1
